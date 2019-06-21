9c90dfa412e69b47ce60559cd51a678a6a2230d8
sennayrton <50055674+sennayrton@users.noreply.github.com>
2019-06-21 17:51:18 +0200

    Update Ej2_4_2.java

--- CursoJava/src/folder/Ej2_4_2.java
+++ CursoJava/src/folder/Ej2_4_2.java
@@ -8,10 +8,7 @@ package Ejercicios2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-/**
- *
- * @author Sergio
- */
+
 public class Ej2_4_2 {
 
     /**
